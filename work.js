9cea08c5834784ee61611fc49b03e089e575928a
qtdwzAdam <wzduchina@gmail.com>
2020-08-10 09:32:05 +0800

    Create work.js

---
+++ work.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        work
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @include  /h42.pm.netease/*
+// @exclude  *://login.netease.com/*
+// @grant        none
+// @updateURL     http://https://github.com/qtdwzAdam/test/raw/master/work.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log("version: 0.2");
+})();

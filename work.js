b4a9d3e0aa1d71c95d97eae56ea170e2686ab3a2
qtdwzAdam <wzduchina@gmail.com>
2020-08-10 09:43:50 +0800

    Update work.js

--- work.js
+++ work.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        work
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      1.2
 // @description  try to take over the world!
 // @author       You
 // @include  /h42.pm.netease/*
@@ -14,5 +14,5 @@
     'use strict';
 
     // Your code here...
-    console.log("version: 0.2");
+    console.log("version: 1.2");
 })();
